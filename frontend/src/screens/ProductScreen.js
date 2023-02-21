f9dd9ea83fc6a887c2481aa469b19531255a89eb
Jazz Abad <abadjazzrell@gmail.com>
2023-02-22 07:07:28 +0800

    Created React app - Add page Routing - Node.js Server - Backend - Reducer Hook - Rating and Product Component - Created Product Details - Loading Message Component

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,125 @@
+import axios from "axios";
+import { useEffect, useReducer } from "react";
+import { Row, Col, ListGroup, Card, Badge, Button } from "react-bootstrap";
+import { Helmet } from "react-helmet-async";
+import { useParams } from "react-router-dom";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import Rating from "../components/Rating";
+
+import { getError } from "../utils";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, product: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+function ProductScreen() {
+  const params = useParams();
+  const { slug } = params;
+
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+    product: [],
+    loading: true,
+    error: "",
+  });
+  //const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get(`/api/products/slug/${slug}`);
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
+      }
+
+      //setProducts(result.data);
+    };
+
+    fetchData();
+  }, [slug]);
+
+  return loading ? (
+    <LoadingBox />
+  ) : error ? (
+    <MessageBox variant='danger'>{error}</MessageBox>
+  ) : (
+    <div className=''>
+      <Row>
+        <Col md={6}>
+          <img
+            src={product.image}
+            alt={product.name}
+            className='img-large'
+          ></img>
+        </Col>
+
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <Helmet>
+                <title>{product.name}</title>
+              </Helmet>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <Rating
+                rating={product.rating}
+                numReviews={product.numReviews}
+              ></Rating>
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: ${product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <Card.Body>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>${product.price}</Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status</Col>
+                    <Col>
+                      {product.countInStock > 0 ? (
+                        <Badge bg='success'>In Stock</Badge>
+                      ) : (
+                        <Badge bg='danger'>Unavailable</Badge>
+                      )}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <div className='d-grid'>
+                      <Button Variant='primary'>Add to Cart</Button>
+                    </div>
+                  </ListGroup.Item>
+                )}
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default ProductScreen;
