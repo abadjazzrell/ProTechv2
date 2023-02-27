681475b4456381ab513189485c6cd4c31439ba01
Jazz Abad <abadjazzrell@gmail.com>
2023-02-28 04:39:57 +0800

    testing cors

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -32,7 +32,9 @@ function HomeScreen() {
     const fetchData = async () => {
       dispatch({ type: "FETCH_REQUEST" });
       try {
-        const result = await axios.get("http://localhost:5002/api/products");
+        const result = await axios.get(
+          "https://capstone-3-p5mm.onrender.com/api/products"
+        );
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
         dispatch({ type: "FETCH_FAIL", payload: err.message });
@@ -42,8 +44,6 @@ function HomeScreen() {
     };
     fetchData();
   }, []);
-
-  console.log(products);
 
   return (
     <div>
